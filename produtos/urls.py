8ebf3500685dd10c7c3e503154ac1222065128aa
Hugo Moisés <hugo2018moises@gmail.com>
2025-06-14 17:09:08 -0300

    Registro e listagem de vendas

--- produtos/urls.py
+++ produtos/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
-from .views import index, produto_list, create_produto, estoque
+from .views import index, produto_list, create_produto, estoque, vendas, create_venda, produto_update, produto_delete
 
 urlpatterns = [
     path('', index, name='index'),
+    #Produtos
     path('produtos/', produto_list, name='produto_list'),
     path('produtos/create/', create_produto, name='create_produto'),
+    path('produtos/update/<int:id>/', produto_update, name='produto_update'),
+    path('produtos/delete/<int:id>/', produto_delete, name='produto_delete'),
+    #Vendas
     path('estoque/', estoque, name='estoque'),
+    path('vendas/', vendas, name='vendas'),
+    path('vendas/create/', create_venda, name='create_venda'),
 ]
--- produtos/views.py
+++ produtos/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import ProdutoForm, ProdutoVenda
 from .models import Produto, Venda
 
 def index(request):
     return render(request, 'produtos/index.html')
+
+#CRUD Produtos
 
 def produto_list(request):
     produtos = Produto.objects.all()
@@ -32,22 +34,66 @@ def create_produto(request):
     }
     return render(request, 'produtos/produtos_create.html', context)
 
+def produto_update(request, id):
+    produto = get_object_or_404(Produto, id=id)
+    if request.method == 'POST':
+        form = ProdutoForm(request.POST, instance=produto)
+        if form.is_valid():
+            form.save()
+            return redirect('produto_list')
+    else:
+        form = ProdutoForm(instance=produto)
+    
+    context = {
+        'form': form,
+        'produto': produto
+    }
+    return render(request, 'produtos/produtos_update.html', context)
+
+def produto_delete(request, id):
+    produto = get_object_or_404(Produto, id=id)
+    produto.delete()
+    return redirect('produto_list')
+    
+
+#Estoque
 def estoque(request):
     produtos = Produto.objects.all()
+    context = {
+        'produtos': produtos
+    }
+    return render(request, 'produtos/estoque.html', context)
+
+
+#CRUD Vendas
+
+def vendas(request):
     vendas = Venda.objects.all()
+    faturamento_total = sum(venda.valor_total() for venda in vendas)
+    context = {
+        'vendas': vendas,
+        'faturamento_total': faturamento_total
+    }
+    return render(request, 'produtos/vendas.html', context)
+
+def create_venda(request):
     if request.method == 'POST':
         form = ProdutoVenda(request.POST)
         if form.is_valid():
             venda = form.save(commit=False)
             produto = venda.produto
-            produto.estoque -= venda.quantidade
-            produto.save()
-            venda.save()
+            if produto.estoque >= venda.quantidade:
+                produto.estoque -= venda.quantidade
+                print(f"Produto: {produto.nome} | Novo estoque: {produto.estoque}")
+                produto.save()
+                venda.save()
+                return redirect('vendas')
+            else:
+                form.add_error('quantidade', 'Quantidade em estoque insuficiente.')
     else:
         form = ProdutoVenda()
+    
     context = {
-        'produtos': produtos,
-        'vendas': vendas,
         'form': form
     }
-    return render(request, 'produtos/estoque.html', context)
+    return render(request, 'produtos/vendas_create.html', context)
